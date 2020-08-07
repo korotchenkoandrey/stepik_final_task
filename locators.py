56ac831c9cc397792cb29377f955a01aeddcf847
Andrey <korotchenkoandrey@gmail.com>
2020-08-07 15:28:30 +0300

    add new page usrl logic and asserts for login/register forms

--- locators.py
+++ locators.py
@@ -3,3 +3,15 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_EMAIL_ENTRY_FIELD = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD_ENTRY_FIELD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BUTTON = (By.NAME, "login_submit")
+    REGISTER_EMAIL_ENTRY_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD_ENTRY_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD_ENTRY_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
